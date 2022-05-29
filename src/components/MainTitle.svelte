f53751a2e306341cc34beec069ca287475245e18
mjespinoza <mjespinoza2@uc.cl>
2022-05-29 18:39:48 -0400

    feat/component title and input

---
+++ src/components/MainTitle.svelte
@@ -0,0 +1,23 @@
+
+<div id="title-grid">
+    <h1 class="title">Weather App</h1>
+</div>
+
+
+<style>
+#title-grid {
+
+  position: relative;
+  width: 100%;
+  margin: 10px;
+  float: left;
+  left: -10px;
+  grid-column: span 2;
+}
+
+.title {
+  justify-content: center;
+  text-align: center;
+  color: black;
+}
+</style>
---
+++ src/components/SearchBar.svelte
@@ -0,0 +1,41 @@
+<script>
+let place="";
+function makeSearch(){
+    
+}
+</script>
+<div id="bar" class="search-wrapper input-group mb-3">
+    <input class="input-search" bind:value={place} type="text" placeholder="Busca un lugar"/>
+    <button class="btn btn-outline-secondary" on:click={makeSearch}>Buscar</button>
+</div>
+
+
+
+<style>
+#bar {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 40vw;
+  height: 10vh;
+  padding: 15px;
+  border-radius: 0.5rem;
+  align-self: center;
+  grid-column: span 2;
+  margin: 15px;
+  margin: auto;
+
+}
+.input-search {
+  width: 75%;
+  height: 100%;
+  font-size: 1.5rem;
+  padding: 10px;
+}
+button {
+  width: 25%;
+  height: 100%;
+  font-size: 1.5rem;
+}
+</style>
